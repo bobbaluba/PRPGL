778e6ec35459487be8e558aeb13e747496d0b8c8
Johan Klokkhammer Helsing <bobbaluba@gmail.com>
2011-04-13 10:15:10 +0200

    example for namegen

---
+++ examples/nameprinter.cpp
@@ -0,0 +1,19 @@
+#include "PRPGL/namegen.h"
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    string line;
+    srand(time(NULL));
+    while(line!="quit"){
+        for(int i=0; i<10; ++i)cout << NameGen::getName() << endl;
+        getline(cin, line);
+    }
+    return 0;
+}
